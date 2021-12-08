0ca12646abdbf1a509a7299439768ffa20091395
fanhefeng <he_feng@ckc.creative.com>
2021-12-08 11:42:23 +0800

    vite element-plus build bug

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	<el-divider>华丽的分隔线</el-divider>
+	<el-button>按钮</el-button>
+</template>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
